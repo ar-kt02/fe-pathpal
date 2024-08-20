6a91239e636b5d9d7d37f2a299b774e5b4412da0
A Katz <akatz@Mac-mini.local>
2024-08-20 14:11:03 +0100

    Setup Pedometer for step tracking, to be integrated with API

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+      ),
+      body: Center(
+        child: Text(
+          "Test settings",
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/stats_page.dart
+++ lib/pages/stats_page.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-// import 'package:pedometer/pedometer.dart';
+import 'package:pedometer/pedometer.dart';
 
-class StatsPage extends StatelessWidget {
+class StatsPage extends StatefulWidget {
   const StatsPage({super.key});
+
+  @override
+  _StatsPageState createState() => _StatsPageState();
+}
+
+class _StatsPageState extends State<StatsPage> {
+  int _stepsCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Pedometer.stepCountStream.listen((stepCount) {
+      setState(() {
+        _stepsCount = stepCount.steps;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Stats")),
-      body: const Center(
-          child: Text('Stats Page Initial', style: TextStyle(fontSize: 30))),
+      appBar: AppBar(
+          title: Text("Stats"), backgroundColor: const Color(0xFFFF9E6E)),
+      body: Center(
+        child: Text('Total Steps: $_stepsCount'),
+      ),
     );
   }
 }
