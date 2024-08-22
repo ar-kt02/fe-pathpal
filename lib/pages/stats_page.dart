1c2105088d857a1b9959a21511f96c51e9724145
Ajmain-Mannan <ajmainmannan@gmail.com>
2024-08-22 17:02:56 +0100

    initial setup for api step tracker

--- lib/pages/stats_page.dart
+++ lib/pages/stats_page.dart
@@ -1,19 +1,66 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:pedometer/pedometer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../utils/api_service.dart';
 
 class StatsPage extends StatefulWidget {
   const StatsPage({super.key});
 
   @override
-  _StatsPageState createState() => _StatsPageState();
+  StatsPageState createState() => StatsPageState();
 }
 
-class _StatsPageState extends State<StatsPage> {
+class StatsPageState extends State<StatsPage> {
   int _stepsCount = 0;
+  int _totalSteps = 0;
+  late String _email;
+  Timer? _patchTimer;
 
   @override
   void initState() {
     super.initState();
+    _accessEmail();
+    _runPedometer();
+    setUpTimedPatch();
+  }
+
+  Future<void> _accessEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _email = prefs.getString("email") ?? "";
+    });
+    if (_email.isNotEmpty) {
+      await _fetchUserInfo();
+    }
+  }
+
+  Future<void> _fetchUserInfo() async {
+    if (_email.isNotEmpty) {
+      final apiService = ApiService();
+      final userInfo = await apiService.fetchUserInfo(_email);
+      if (userInfo != null) {
+        setState(() {
+          _totalSteps = userInfo["step_details"]?["total_steps"];
+        });
+      }
+    }
+  }
+
+  void setUpTimedPatch() {
+    _patchTimer =
+        Timer.periodic(const Duration(hours: 1), (_) => _hourlyPatchSteps());
+  }
+
+  Future<void> _hourlyPatchSteps() async {
+    if (_email.isNotEmpty) {
+      final apiService = ApiService();
+      await apiService.patchTodaySteps(_email, _stepsCount);
+    }
+  }
+
+  void _runPedometer() {
     Pedometer.stepCountStream.listen((stepCount) {
       setState(() {
         _stepsCount = stepCount.steps;
@@ -22,12 +69,18 @@ class _StatsPageState extends State<StatsPage> {
   }
 
   @override
+  void dispose() {
+    _patchTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Stats"), backgroundColor: const Color(0xFFFF9E6E)),
+          title: const Text("Stats"), backgroundColor: const Color(0xFFFF9E6E)),
       body: Center(
-        child: Text('Total Steps: $_stepsCount'),
+        child: Text('Todays Steps: $_stepsCount, Total Steps: $_totalSteps'),
       ),
     );
   }
--- lib/utils/api_service.dart
+++ lib/utils/api_service.dart
@@ -12,4 +12,26 @@ class ApiService {
       return false;
     }
   }
+
+  Future<Map<String, dynamic>?> fetchUserInfo(String email) async {
+    try {
+      final response = await _dio.get('$_baseURL/users/$email');
+      if (response.statusCode == 200) {
+        return response.data;
+      }
+    } catch (e) {
+      return null;
+    }
+    return null;
+  }
+
+  Future<void> patchTodaySteps(String email, int todaySteps) async {
+    try {
+      await _dio.patch('$_baseURL/users/$email', data: {
+        'step_details': {'todays_steps': todaySteps}
+      });
+    } catch (e) {
+      return;
+    }
+  }
 }
